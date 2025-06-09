a1978016f1e8f6d64003734162dc8e19e043a51f
khushii-agrawal <khushipremy789@gmail.com>
2025-06-09 18:35:22 +0530

    login route remove

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
+        
       </Routes>
     </Router>
   );
