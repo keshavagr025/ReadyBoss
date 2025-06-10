d16e2353a7600d5d7667df3bbe61465466ae933a
khushalkks <sahukks3101@gmail.com>
2025-06-10 14:03:24 +0530

    add changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+// App.jsx
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Feature from "./components/Feature";
@@ -7,25 +9,34 @@ import Testimonials from "./components/Testimonials";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 import Hero from "./components/Hero";
+import UploadResume from "./components/UploadResume/UploadResume";
 
-const Home = () => (
+const Home = ({ isLoggedIn }) => (
   <>
     <Navbar />
     <Hero />
     <About />
     <Feature />
     <Testimonials />
-
+    {/* <UploadResume isLoggedIn={isLoggedIn} /> */}
   </>
 );
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Ye function signup/login success ke baad call hoga to update login state
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home isLoggedIn={isLoggedIn} />} />
+        <Route path="/signup" element={<SignUp onLogin={handleLogin} />} />
+        <Route path="/login" element={<Login onLogin={handleLogin} />} />
+        <Route path="/upload-resume" element={<UploadResume />} />
       </Routes>
     </Router>
   );
